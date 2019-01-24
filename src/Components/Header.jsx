ab5a752365eed1d35ec53ced5463fa28b88314cd
Mark Ma <mazheng1022@hotmail.com>
2019-01-24 14:04:03 -0500

    initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render(){
+        return (
+            <header>
+                <div className = "logo">
+                </div>
+                <div className = "navigation">
+                    <li className="navs" id="homeBtn"><i className="fas fa-home"></i>Home</li>
+                    <li  className="navs" id="aboutBtn"><i className="fas fa-id-card"></i>About</li>
+                    <li className="navs" id="workBtn"><i className="fas fa-pencil-alt"></i>Work</li>
+                    <li className="navs" id="contactBtn"><i className="fas fa-phone"></i>Contact</li>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
