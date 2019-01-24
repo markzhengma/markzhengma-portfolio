97cbd015dbcb375b875382abb3a71cb872292b80
Mark Ma <mazheng1022@hotmail.com>
2019-01-24 14:08:33 -0500

    add home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Home from './Components/Home';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <div className='background-img'></div>
         <Header/>
+        <Home/>
       </div>
     );
   }
---
+++ src/Components/Home.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render(){
+        return (
+            <div className="home">
+                <div className = 'home-box-spacer'>
+                </div>
+                <div className = 'home-box'>
+                    <div className='word-box'>
+                        <h2>Hi. I'm Mark</h2>
+                        <p>Web Developer</p>
+                        <p>Unity Developer</p>
+                        <p>Teacher</p>
+                    </div>
+                    <div className="project" id = 'latest-project'>
+                        <div className='project-name' id = 'latest-name'>
+                            <h3>Recent Project</h3>
+                        </div>
+                        <div className='project-pic project-pic-15' id = 'latest-pic'>
+                            <div className='tech-used tech-15' id = 'latest-tech'>
+                                <h5>Technology:</h5>
+                                <p>Unity<br/>C#<br/>HTC Plugin</p>
+                            </div>
+                        </div>
+                        <div className='project-name' id = 'latest-name'>
+                            <h5>Witch's Grotto</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
