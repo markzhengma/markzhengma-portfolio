841f055759e11fb1e34038534481f399f0ee8447
Mark Ma <mazheng1022@hotmail.com>
2019-01-29 16:41:48 -0500

    test contact background

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,20 +4,22 @@ import { Element } from 'react-scroll';
 class Contact extends Component {
     render(){
         return (
-            <div className='contact' 
-                // ref = {this.props.contactRef}
-            >
-                <div className = "contact-container">
-                    <Element className="spacer contact-spacer" name = "contact-spacer"/>
-                    <h2><i className="fa fa-phone fa-fw" aria-hidden="true"></i>Contact Me</h2>
-                    <form className='contact-form' method="POST" action="http://formspree.io/mazheng1022@hotmail.com">
-                        <input type="email" name="email" placeholder="Your Email"/>
-                        <textarea name="message" placeholder="Please leave your message."></textarea>
-                        <button type="submit">Send</button>
-                    </form>
-                    <div className="spacer contact-spacer"/>
-                </div>
-            </div> 
+            <div>
+                <div className='contact' 
+                    // ref = {this.props.contactRef}
+                >
+                    <div className = "contact-container">
+                        <Element className="spacer contact-spacer" name = "contact-spacer"/>
+                        <h2><i className="fa fa-phone fa-fw" aria-hidden="true"></i>Contact Me</h2>
+                        <form className='contact-form' method="POST" action="http://formspree.io/mazheng1022@hotmail.com">
+                            <input type="email" name="email" placeholder="Your Email"/>
+                            <textarea name="message" placeholder="Please leave your message."></textarea>
+                            <button type="submit">Send</button>
+                        </form>
+                    </div>
+                </div> 
+                {/* <div className="spacer contact-spacer"/> */}
+            </div>
         )
     }
 }
