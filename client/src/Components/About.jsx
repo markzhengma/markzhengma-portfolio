b5425b18888d93d9102c76ac4ef7892a9786a71d
Mark Ma <mazheng1022@hotmail.com>
2019-03-19 12:28:16 -0400

    reorganize about page. modified skill icons

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -10,6 +10,50 @@ class About extends Component {
                 <Element className="spacer about-spacer" name = "about-spacer"/>
                 <h2><i className="fa fa-id-card fa-fw" aria-hidden="true"></i>About Me</h2>
                 <div className='paragraph-list'>
+                    <div className={`paragraphs ${this.props.showParaId.includes("p5") ? `selection` : ``}`} id='p5' 
+                        onClick = {() => this.props.showOrHideParagraph("p5")}>
+                        <div className={`paragraph-title title-p5 ${this.props.showParaId.includes("p5") ? window.innerWidth >= 1200 ? `title-hide` : `` : ``}`}>
+                            <h2>Skills</h2>
+                        </div>
+                        <div className='image img-5'>
+                        </div>
+                        <div className={`paragraph-single single-p5 ${this.props.showParaId.includes("p5") ? window.innerWidth >= 1200 ? `paragraph-show` : `` : ``}`}>
+                            <div className='skill-single skill-html'><div className='skill-name'>HTML</div></div>
+                            <div className='skill-single skill-css'><div className='skill-name'>CSS</div></div>
+                            <div className='skill-single skill-js'><div className='skill-name'>Javascript</div></div>
+                            <div className='skill-single skill-sql'><div className='skill-name'>SQL</div></div>
+                            <div className='skill-single skill-git'><div className='skill-name'>Git</div></div>
+                            <div className='skill-single skill-react'><div className='skill-name'>React</div></div>
+                            <div className='skill-single skill-node'><div className='skill-name'>Node</div></div>
+                            <div className='skill-single skill-express'><div className='skill-name'>Express</div></div>
+                            <div className='skill-single skill-socket'><div className='skill-name'>Socket</div></div>
+                            <div className='skill-single skill-ruby'><div className='skill-name'>Ruby</div></div>
+                            <div className='skill-single skill-java'><div className='skill-name'>Java</div></div>
+                            <div className='skill-single skill-android'><div className='skill-name'>Android<br />Studio</div></div>
+                            <div className='skill-single skill-csharp'><div className='skill-name'>C#</div></div>
+                            <div className='skill-single skill-unity'><div className='skill-name'>Unity</div></div>
+                            <div className='skill-single skill-vive'><div className='skill-name'>Vive</div></div>
+                            <div className='skill-single skill-vuforia'><div className='skill-name'>Vuforia</div></div>
+                        </div>
+                    </div>
+                    <div className={`scroll scroll-p5 ${this.props.showParaId.includes("p5") ? window.innerWidth < 1200 ? `scroll-show` : `` : ``}`}>
+                        <div className='skill-single skill-html'><div className='skill-name'>HTML</div></div>
+                        <div className='skill-single skill-css'><div className='skill-name'>CSS</div></div>
+                        <div className='skill-single skill-js'><div className='skill-name'>Javascript</div></div>
+                        <div className='skill-single skill-sql'><div className='skill-name'>SQL</div></div>
+                        <div className='skill-single skill-git'><div className='skill-name'>Git</div></div>
+                        <div className='skill-single skill-react'><div className='skill-name'>ReactJS</div></div>
+                        <div className='skill-single skill-node'><div className='skill-name'>NodeJS</div></div>
+                        <div className='skill-single skill-express'><div className='skill-name'>ExpressJS</div></div>
+                        <div className='skill-single skill-socket'><div className='skill-name'>Socket</div></div>
+                        <div className='skill-single skill-ruby'><div className='skill-name'>Ruby</div></div>
+                        <div className='skill-single skill-java'><div className='skill-name'>Java</div></div>
+                        <div className='skill-single skill-android'><div className='skill-name'>Android<br />Studio</div></div>
+                        <div className='skill-single skill-csharp'><div className='skill-name'>C#</div></div>
+                        <div className='skill-single skill-unity'><div className='skill-name'>Unity</div></div>
+                        <div className='skill-single skill-vive'><div className='skill-name'>Vive</div></div>
+                        <div className='skill-single skill-vuforia'><div className='skill-name'>Vuforia</div></div>
+                    </div>
                     <div className={`paragraphs ${this.props.showParaId.includes("p1") ? `selection` : ``}`} id="p1" 
                         onClick = {() => this.props.showOrHideParagraph("p1")}>
                         <div className={`paragraph-title title-p1 ${this.props.showParaId.includes("p1") ? window.innerWidth >= 1200 ? `title-hide` : `` : ``}`}>
@@ -82,48 +126,7 @@ class About extends Component {
                             In Beijing, China, Mandarin teacher in RICH summer camps and volunteer teacher for a non-profit educational orgnization INCLUDED with students of grade 2-3. Also, I worked as a pre-school teacher at River School in Jersey City and a Mandarin teacher at Hudsonway Immersion School in NYC.
                         </p>
                     </div>
-                    <div className={`paragraphs ${this.props.showParaId.includes("p5") ? `selection` : ``}`} id='p5' 
-                        onClick = {() => this.props.showOrHideParagraph("p5")}>
-                        <div className={`paragraph-title title-p5 ${this.props.showParaId.includes("p5") ? window.innerWidth >= 1200 ? `title-hide` : `` : ``}`}>
-                            <h2>Skills</h2>
-                        </div>
-                        <div className='image img-5'>
-                        </div>
-                        <div className={`paragraph-single single-p5 ${this.props.showParaId.includes("p5") ? window.innerWidth >= 1200 ? `paragraph-show` : `` : ``}`}>
-                            <div className='skill-single skill-html'><div className='skill-name'>HTML</div></div>
-                            <div className='skill-single skill-css'><div className='skill-name'>CSS</div></div>
-                            <div className='skill-single skill-js'><div className='skill-name'>Javascript</div></div>
-                            <div className='skill-single skill-react'><div className='skill-name'>React</div></div>
-                            <div className='skill-single skill-node'><div className='skill-name'>Node</div></div>
-                            <div className='skill-single skill-express'><div className='skill-name'>Express</div></div>
-                            <div className='skill-single skill-sql'><div className='skill-name'>SQL</div></div>
-                            <div className='skill-single skill-ruby'><div className='skill-name'>Ruby</div></div>
-                            <div className='skill-single skill-socket'><div className='skill-name'>Socket</div></div>
-                            <div className='skill-single skill-java'><div className='skill-name'>Java</div></div>
-                            <div className='skill-single skill-csharp'><div className='skill-name'>C#</div></div>
-                            <div className='skill-single skill-unity'><div className='skill-name'>Unity</div></div>
-                            <div className='skill-single skill-android'><div className='skill-name'>Android<br />Studio</div></div>
-                            <div className='skill-single skill-vive'><div className='skill-name'>Vive</div></div>
-                            <div className='skill-single skill-vuforia'><div className='skill-name'>Vuforia</div></div>
-                        </div>
-                    </div>
-                    <div className={`scroll scroll-p5 ${this.props.showParaId.includes("p5") ? window.innerWidth < 1200 ? `scroll-show` : `` : ``}`}>
-                        <div className='skill-single skill-html'><div className='skill-name'>HTML</div></div>
-                        <div className='skill-single skill-css'><div className='skill-name'>CSS</div></div>
-                        <div className='skill-single skill-js'><div className='skill-name'>Javascript</div></div>
-                        <div className='skill-single skill-react'><div className='skill-name'>ReactJS</div></div>
-                        <div className='skill-single skill-node'><div className='skill-name'>NodeJS</div></div>
-                        <div className='skill-single skill-express'><div className='skill-name'>ExpressJS</div></div>
-                        <div className='skill-single skill-sql'><div className='skill-name'>SQL</div></div>
-                        <div className='skill-single skill-ruby'><div className='skill-name'>Ruby</div></div>
-                        <div className='skill-single skill-socket'><div className='skill-name'>SocketIO</div></div>
-                        <div className='skill-single skill-java'><div className='skill-name'>Java</div></div>
-                        <div className='skill-single skill-csharp'><div className='skill-name'>C#</div></div>
-                        <div className='skill-single skill-unity'><div className='skill-name'>Unity</div></div>
-                        <div className='skill-single skill-android'><div className='skill-name'>Android<br />Studio</div></div>
-                        <div className='skill-single skill-vive'><div className='skill-name'>Vive</div></div>
-                        <div className='skill-single skill-vuforia'><div className='skill-name'>Vuforia</div></div>
-                    </div>
+                    
             </div>
         </div>
         )
